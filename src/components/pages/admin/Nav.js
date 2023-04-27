eca239437bcdddfa10b4f3c0bfd49a24a7d5190e
Nadir Ouzlim <ouzlimnadir@gmail.com>
2023-04-27 19:47:17 +0100

    Ajusting design

--- src/components/pages/admin/Nav.js
+++ src/components/pages/admin/Nav.js
@@ -3,8 +3,8 @@ import 'bootstrap/js/dist/collapse';
 function Nav(props) {
     let username=props.fullname;
     return(
-    <nav className="navbar navbar-expand-sm navbar-dark bg-transparent ">
-            <i className="navbar-brand bi bi-justify-left fs-4 " onClick={props.Toggle}/>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-transparent mx-lg-2 my-lg-2 ">
+            <i className="navbar-brand bi bi-justify-left fs-4  " onClick={props.Toggle}/>
             <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
                     aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <i className="bi bi-justify"/>
